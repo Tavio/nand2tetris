27141c2ec12e872a8fedf270cd766f0f3b18e480
Otavio Nascimento <otanasc@gmail.com>
2019-03-17 17:43:44 +1100

    Fill exercise

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BEGIN)
+
+  @KBD
+  D=M
+  @BLACKCOLOR
+  D;JGT
+
+  @color
+  M=0
+  @PAINT
+  0;JMP
+
+  (BLACKCOLOR)
+    @color
+    M=-1
+
+  (PAINT)
+    @SCREEN
+    D=A
+    @rowaddr
+    M=D
+
+    @i
+    M=0
+
+  (ROWLOOP)
+    @j
+    M=0
+
+    @i
+    D=M
+    @255
+    D=D-A
+    @END
+    D;JGT
+
+    @rowaddr
+    D=M
+    @coladdr
+    M=D
+
+    (COLLOOP)
+      @j
+      D=M
+      @31
+      D=D-A
+      @ROWLOOPCONT
+      D;JGT
+
+      @color
+      D=M
+      @coladdr
+      A=M
+      M=D
+
+      @j
+      M=M+1
+      @coladdr
+      M=M+1
+      @COLLOOP
+      0;JMP
+
+  (ROWLOOPCONT)
+    @i
+    M=M+1
+    @32
+    D=A
+    @rowaddr
+    M=D+M
+    @ROWLOOP
+    0;JMP
+
+(END)
+  @BEGIN
+  0;JMP
